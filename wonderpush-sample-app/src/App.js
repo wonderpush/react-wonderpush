6194ed102d2683c47f2695928725238939ea6c92
drameBoubacar <boubacardrame9@gmail.com>
2018-12-04 16:54:20 +0000

    implement react-cli

---
+++ wonderpush-sample-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import ReactWP from 'react-wonderpush';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>wonderpsuh sample app</h1>
+      </div>
+    );
+  }
+}
+
+export default App;

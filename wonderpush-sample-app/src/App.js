d4e35a62c0a8050414f48de2883a4710b34c2867
drameBoubacar <boubacardrame9@gmail.com>
2018-12-06 16:17:52 +0000

    added init

--- wonderpush-sample-app/src/App.js
+++ wonderpush-sample-app/src/App.js
@@ -1,32 +1,47 @@
 import React, { Component } from 'react';
-import ReactWP from 'react-wonderpush';
+import WPEventList from './components/WPEventList';
+import SwitchButton from './components/SwitchButton';
+import './App.css';
+import WonderPush from 'react-wonderpush';
 
-const wrapper = ReactWP.getInstance('head');
 
-wrapper.init("a6a00ccf60ffee04cb201cead4304fc4db8fa8e0a2823a77466e8c8ebeed4e25", {
-  // You can modify or translate the following:
-  externalBoxMessage: "We'd like to send you notifications",
-  externalBoxExampleTitle: "Example notification",
-  externalBoxExampleMessage: "This is an example notification",
-  externalBoxDisclaimer: "You can always unsubscribe at anytime.",
-  externalBoxProcessingMessage: "Subscribing...",
-  externalBoxSuccessMessage: "Thanks for subscribing!",
-  externalBoxFailureMessage: "Sorry, something went wrong.",
-  externalBoxTooLongHint: "Poor connection or private browsing?",
-  externalBoxCloseHint: "Close",
-  modalBoxMessage: "We will send you personalized notifications.<br/>You can always unsubscribe at any time.",
-  modalBoxButton: "Got it!"
+WonderPush.init({
+  webKey: "a6a00ccf60ffee04cb201cead4304fc4db8fa8e0a2823a77466e8c8ebeed4e25",
+  applicationName:        'React WonderPush Demo',
+  notificationDefaultUrl: 'https://www.wonderpush.com/demo/',
+  notificationIcon:       '../img/image.png'
 });
-
 class App extends Component {
   constructor(){
     super();
-    console.log('APP REACT INITIALIZED');
+    console.log('APP REACT INITIALIZED' );
+  }
+  componentDidMount(){
+    WonderPush.ready(function(WonderPushSDK) {
+      WonderPushSDK.Notification.setOptInOptions({
+        modalBoxMessage: "We will send you personalized notifications.<br/>You can always unsubscribe at any time.",
+        modalBoxButton: "Got it!",
+        externalBoxProcessingMessage: "Subscribing...",
+        externalBoxSuccessMessage: "Thanks for subscribing!",
+        externalBoxFailureMessage: "Sorry, something went wrong.",
+        externalBoxTooLongHint: "Poor connection or private browsing?",
+        externalBoxCloseHint: "Close"
+      });
+      if (WonderPushSDK.isNativePushNotificationSupported()) {
+          // For best results, test if:
+          // - user engagement is sufficient
+          // - user is willing to accept notifications,
+          //   using a non intrusive information message
+          WonderPushSDK.setNotificationEnabled(true);
+      }
+    });
   }
   render() {
     return (
       <div className="App">
-      <h1>wonderpsuh sample app</h1>
+      <h1>Connecting to wonderpush</h1>
+      <SwitchButton/>
+      <WPEventList/>
       </div>
     );
   }

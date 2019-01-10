f8276ff92be6ddbe5229b4f52ec8452c316beb0f
jgrach <julien@grach.fr>
2019-01-10 15:41:25 +0100

    withwonderpush

--- src/withWonderPush.js
+++ src/withWonderPush.js
@@ -1,9 +1,18 @@
 import React,{Component} from 'react';
+import WonderPush from './WonderPush';
 
 export default function withWonderPush (OriginalComponent) {
   class WonderpushHOC extends Component {
+    constructor(){
+      this.state = { wp }
+    }
+
+    componentWillMount(){
+      WonderPush.ready( wp => this.setState(wp))
+    }
+
     render(){
-      return <OriginalComponent {...this.props}/>
+      return <OriginalComponent wp={this.state.wp} {...this.props}/>
     }
   }
   return WonderpushHOC;

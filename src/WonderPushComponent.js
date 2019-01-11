cb17e99855102276b054578de1ce7418e8980263
jgrach <julien@grach.fr>
2019-01-11 11:57:33 +0100

    with commentary

--- src/WonderPushComponent.js
+++ src/WonderPushComponent.js
@@ -1,6 +1,16 @@
 import React,{Component} from 'react';
 import WonderPush from './WonderPush';
 
+/* 
+  Un provider pour initialiser WonderPush
+  Etape obligatoire.
+  Par intuition on aurait aimé que s'y trouve également le .ready() et la transmission de wonderpush,
+  on aurait récupéré l'objet par consumer.
+  Mais pour éviter de bloquer toute l'application de l'utilisateur, il faut séparer les deux,
+  l'init a lieu au démarrage de l'appli,
+  le ready sera vérifié pour chaque composant nécessitant l'utilisation de wonderpush.
+*/ 
+
 export default class Wonderpush extends Component {
   constructor(props){
     super(props);
--- src/withWonderPush.js
+++ src/withWonderPush.js
@@ -1,6 +1,12 @@
 import React,{Component} from 'react';
 import WonderPush from './WonderPush';
 
+/* 
+  Un high order pour vérifier l'état de l'objet wonderpush et le fournir aux élements enfants.
+  Par sécurité, on devrait envisager l'exécution d'un setTimeOut sur le ready pour ne pas bloquer l'affichage de composants
+  si wonderpush n'est jamais ready.
+*/ 
+
 export default function withWonderPush (OriginalComponent) {
   class WonderpushHOC extends Component {
     constructor(props){

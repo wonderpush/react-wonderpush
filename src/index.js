aed02a3f45afc6a9656ce20e763b01a9c4ead8f5
devMarius <46221716+devMarius@users.noreply.github.com>
2023-04-28 18:47:59 +0200

    Delete index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import WonderPush from './WonderPush.js';
-import withWonderPush from './withWonderPush.js';
-import WonderPushComponent from './WonderPushComponent.js';
-
-export default WonderPush;
-
-export {WonderPushComponent as WonderPush, withWonderPush};

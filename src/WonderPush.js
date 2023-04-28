8c3bb8036de3f9a3b8681a49b208855edbb88415
devMarius <46221716+devMarius@users.noreply.github.com>
2023-04-28 18:46:41 +0200

    Delete WonderPush.js

--- src/WonderPush.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    var s = document.createElement('script');
-    s.src = "https://cdn.by.wonderpush.com/sdk/1.1/wonderpush-loader.min.js";
-    document.head.appendChild(s);
-})();
-
-window.WonderPush = window.WonderPush || [];
-export default WonderPush;

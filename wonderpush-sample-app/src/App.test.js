fc9489342dea810114d8725d2396e6ebd71d2598
devMarius <46221716+devMarius@users.noreply.github.com>
2023-04-28 18:56:56 +0200

    Delete wonderpush-sample-app directory

--- wonderpush-sample-app/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-it('renders without crashing', () => {
-  const root = createRoot(document.getElementById('root'));
-  root.render(<App />);
-  root.unmount();
-});

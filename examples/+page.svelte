b9d32b758bcc245d084eacb00b8f64480467ab30
devMarius <46221716+devMarius@users.noreply.github.com>
2023-04-28 19:03:30 +0200

    Create +page.svelte

---
+++ examples/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import WonderPush from "../lib/WonderPush.svelte";
+  import { WonderPushState } from "../lib/WonderPushStore";
+</script>
+
+<WonderPush options={{ ...yourWonderPushOptions }}></WonderPush>
+
+{#if $WonderPushState}
+  <!-- Put your content or components that require WonderPush to be ready here -->
+  <h1>WonderPush is ready!</h1>
+{/if}
